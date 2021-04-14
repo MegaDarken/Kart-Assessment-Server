0821e869b7a3d2012dfd8cc872c2bcc767e8ea87
MegaDarken <joe.swales@googlemail.com>
2021-04-15 00:22:36 +0100

    Added LoopingThread runnable class

---
+++ src/LoopingThread.java
@@ -0,0 +1,34 @@
+//File: LoopingThread.java
+import java.lang.*;
+import java.util.concurrent.*;
+
+public class LoopingThread implements Runnable
+{
+   private boolean _looping;
+   
+   public LoopingThread()
+   {
+      _looping = true;
+      
+   }
+
+   public void run()
+   {
+   
+      //Loopfor duration of runtime
+      while(_looping)
+      {
+      
+         //Process World
+         
+         
+         
+         
+         //Sleep
+         
+      }
+      
+   
+   }
+
+}
